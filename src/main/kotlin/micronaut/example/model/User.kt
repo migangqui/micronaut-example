207889e694c2cdf0f00c7c0a6959bf440aa5fb7f
miguel <maq1993@gmail.com>
2018-10-09 11:54:38 +0200

    User no args constructor

--- src/main/kotlin/micronaut/example/model/User.kt
+++ src/main/kotlin/micronaut/example/model/User.kt
@@ -2,6 +2,4 @@ package micronaut.example.model
 
 import org.bson.types.ObjectId
 
-data class User (var _id: ObjectId?, var name: String, var surname: String, var job: String, var age: Int) {
-    constructor(): this(null, "", "", "", 0)
-}
+data class User (var _id: ObjectId? = null, var name: String = "", var surname: String = "", var job: String = "", var age: Int = 0)
