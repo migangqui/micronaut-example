fac8b937ced151ec037fa66b4ee6fd76c3679db3
miguel <maq1993@gmail.com>
2018-10-09 10:04:42 +0200

    Fix getAll

--- src/main/kotlin/micronaut/example/model/User.kt
+++ src/main/kotlin/micronaut/example/model/User.kt
@@ -2,4 +2,6 @@ package micronaut.example.model
 
 import org.bson.types.ObjectId
 
-data class User (var _id: ObjectId?, var name: String, var surname: String, var job: String, var age: Int)
+data class User (var _id: ObjectId?, var name: String, var surname: String, var job: String, var age: Int) {
+    constructor(): this(null, "", "", "", 0)
+}
